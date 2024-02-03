b7e9e3832ce093993bed32dc9be63cd5a54f6132
oreopr0 <123789155+oreopr0@users.noreply.github.com>
2024-02-03 05:36:52 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -43,7 +43,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheTeamAlexa/AlexaMusic",
+    "https://github.com/oreopr0/AlexaMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
